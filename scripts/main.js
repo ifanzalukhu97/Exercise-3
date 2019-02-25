6ffa071dfc18e8b191c37a0d6c0f520fb26a1614
ifanzalukhu97 <ivan.zalukhu97@gmail.com>
2019-02-25 22:24:52 +0700

    Add template view

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+let tBody = document.getElementById("table-body");
+let dropDownItems = document.querySelectorAll(".dropdown-item");
+var dropDownButton = document.querySelector(".dropdown-toggle");
+
+// Load planet_list object from planet_list.js
+let planetList = planet_list;
